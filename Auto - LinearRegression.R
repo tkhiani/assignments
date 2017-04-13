c1bb5de882764d6e3fd64f34462b74d93d130fbf
Tarun Kamal Khiani <tarun.kamal.khiani@sap.com>
2017-04-13 22:22:07 +0530

    PCA & Linear Regression

--- Auto - LinearRegression.R
+++ Auto - LinearRegression.R
@@ -17,12 +17,15 @@ anova(a.fit1)
 # variance inflation factor
 round(vif(a.fit1),2) 
 
+# linear regression model
 a.fit2 <- lm(mpg ~   cylinders + displacement + weight + acceleration, data=Auto)
 summary(a.fit2)
 anova(a.fit2)
 
+# variance inflation factor
 round(vif(a.fit2),2)
 
+# Exclude the variable that thas the highest variance inflation factor
 a.fit3 <- lm(mpg ~ displacement + weight + acceleration, data=Auto)
 summary(a.fit3)
 round(vif(a.fit3),2)
@@ -31,15 +34,20 @@ a.fit4 <- lm(mpg ~ weight  + acceleration, data=Auto)
 summary(a.fit4)
 round(vif(a.fit4),2)
 
+# Predict using the dependent variables
 AutoTest <- read_excel("./data/AutoTest.xlsx")
 View(AutoTest)
 
 a.fit4.prediction <- predict(a.fit4, AutoTest, interval = "prediction")
 a.fit4.prediction
+
+# Plot the predicted and actual value to check for accuracy of prediction
+predictionAndActuals <- data.frame(a.fit4.prediction[,1], AutoTest$mpg)
+colnames(predictionAndActuals) <- c("Predicted mpg","Actual mpg")
+matplot(predictionAndActuals, type = "l")
+predictionAndActuals
 matplot(a.fit4.prediction, type = "l")
 
 a.fit4.confidenceForMean <- predict(a.fit4, AutoTest, interval = "confidence")
 a.fit4.confidenceForMean
 matplot(a.fit4.confidenceForMean, type = "l")
-
-
--- PCA - Auto Example.R
+++ PCA - Auto Example.R
@@ -2,36 +2,120 @@ library(readxl)
 library(car)
 
 # Import Auto.xlsx
-Auto <- read_excel("~/Documents/Tarun/Tarun@Personal/MyProjects/assignments/data/Auto.xlsx")
-View(Auto)
+car_data <- read_excel("./data/Auto.xlsx")
+View(car_data)
 
+# Correlation across variables
+cor(car_data)
 
-AutoX <- Auto[,-1] # Predictor matrix
+# Determine if we need to scale the data so that the variance is 1
+var(car_data)
 
-round(var(AutoX),2)
+#Scale data
+sCarData <- data.frame(scale(car_data))
+var(sCarData)
 
-# Principal component analysis
-a.pc.1 <- prcomp(AutoX, scale=T) # scaling recommended
-plot(a.pc.1, type='l', main ="Scree Plot of Auto Data")
-summary(a.pc.1)
-a.pc.1$rotation
+#conducting PCA 
+car_pca = prcomp(sCarData[,-1])
 
-SAuto <- scale(Auto)
-SAuto <- data.frame(SAuto)
+#view PCA
+summary(car_pca)
 
+#understand the equation of the PC components 
+car_pca$rotation
 
+#scree Plot to look at the relationship between variables
 xl <- seq(-3, 3)
 yl <- seq(-3, 3)
 plot(xl, yl, type="n", xlab="", ylab="")
-points(SAuto$displacement, SAuto$horsepower, pch=19, cex=0.8, col="forestgreen")
-points(SAuto$weight, SAuto$acceleration, pch=12, cex=0.6, col="blue")
-points(a.pc.1$x[,1], a.pc.1$x[,2], pch=10, cex=0.8, col="red")
+points(sCarData$displacement, sCarData$horsepower, pch=19, cex=0.8, col="forestgreen")
+points(sCarData$weight, sCarData$acceleration, pch=12, cex=0.6, col="blue")
+points(car_pca$x[,1], car_pca$x[,2], pch=10, cex=0.8, col="red")
 title ("Comparison of Original Data and PC Scores")
 
-# Principal component regression
-Auto1 <- cbind(Auto[,1], a.pc.1$x[,1], a.pc.1$x[,2])
-Auto1 <- data.frame(Auto1)
-a.fit5 <- lm(Auto1[,1] ~ Auto1[,2] + Auto1[,3])
-summary(a.fit5)
+# train data
+car_train = cbind(car_data$mpg,car_pca$x[,1],car_pca$x[,2])
+car_train = as.data.frame(car_train)
+names(car_train) = c("mpg","PC1","PC2")
 
-vif(a.fit5)
+#View train data
+View(car_train)
+
+#create linear regression model on train data
+car_pca_lm = lm(mpg~., data = car_train)
+
+#View lm model
+car_pca_lm
+
+#Summary lm model
+summary(car_pca_lm)
+
+# Durbin-Watson Statistic, D = 0 if there is a strong relationship of residual with time else it is close to 2, alternate there is a relationship with time
+dwt(car_pca_lm)
+
+# Normality test for residual, null hypothesis: normally distributed
+shapiro.test(car_pca_lm$residuals)
+
+# Does residual have equal variance between residual and every value of x
+plot(car_train$PC1 ,car_pca_lm$residuals)
+plot(car_train$PC2 ,car_pca_lm$residuals)
+
+#Variance Analysis
+vif(car_pca_lm)
+
+#Prepare to be in a position to test the data
+cylinders_pc1 <- car_pca$rotation[1,1]
+cylinders_pc2 <- car_pca$rotation[1,2]
+cylinders_mean <- mean(car_data$cylinders)
+cylinders_sd <- sd(car_data$cylinders)
+
+displacement_pc1 <- car_pca$rotation[2,1]
+displacement_pc2 <- car_pca$rotation[2,2]
+displacement_mean <- mean(car_data$displacement)
+displacement_sd <- sd(car_data$displacement)
+
+horsepower_pc1 <- car_pca$rotation[3,1]
+horsepower_pc2 <- car_pca$rotation[3,2]
+horsepower_mean <- mean(car_data$horsepower)
+horsepower_sd <- sd(car_data$horsepower)
+
+weight_pc1 <- car_pca$rotation[4,1]
+weight_pc2 <- car_pca$rotation[4,2]
+weight_mean <- mean(car_data$weight)
+weight_sd <- sd(car_data$weight)
+
+acceleration_pc1 <- car_pca$rotation[5,1]
+acceleration_pc2 <- car_pca$rotation[5,2]
+acceleration_mean <- mean(car_data$acceleration) 
+acceleration_sd <- sd(car_data$acceleration)
+
+# creating test data
+AutoTest <- read_excel("./data/AutoTest.xlsx")
+
+# creating test data based on PCA scores
+PC1 <- ( (AutoTest$cylinders-cylinders_mean)/cylinders_sd * cylinders_pc1 + 
+        (AutoTest$displacement-displacement_mean)/displacement_sd * displacement_pc1 +
+          (AutoTest$horsepower-horsepower_mean)/horsepower_sd * horsepower_pc1 +
+            (AutoTest$weight-weight_mean)/weight_sd * weight_pc1 +
+              (AutoTest$acceleration-acceleration_mean)/acceleration_sd * acceleration_pc1 )
+
+PC2 <- ( (AutoTest$cylinders-cylinders_mean)/cylinders_sd * cylinders_pc2 + 
+           (AutoTest$displacement-displacement_mean)/displacement_sd * displacement_pc2 +
+             (AutoTest$horsepower-horsepower_mean)/horsepower_sd * horsepower_pc2 +
+               (AutoTest$weight-weight_mean)/weight_sd * weight_pc2 +
+                 (AutoTest$acceleration-acceleration_mean)/acceleration_sd * acceleratio_pc2 )
+
+# PC1: -0.027740807	-3.197059656	2.335436269
+PC1
+# PC2: 1.847547323	1.185961153	0.140719857
+PC2
+
+#Predict values for test data
+predicted_mpg = predict(car_pca_lm,data.frame(PC1, PC2))
+
+#View predicted data
+predicted_mpg
+predictionAndActuals <- data.frame(predicted_mpg, AutoTest$mpg)
+colnames(predictionAndActuals) <- c("Predicted mpg","Actual mpg")
+predictionAndActuals
+matplot(predictionAndActuals, type = "l")
