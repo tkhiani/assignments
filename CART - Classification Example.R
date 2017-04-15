3164f1476607c8e855935697350998d04c9b8f09
Tarun Kamal Khiani <tarun.kamal.khiani@sap.com>
2017-04-15 13:51:42 +0530

    Classification Example

---
+++ CART - Classification Example.R
@@ -0,0 +1,178 @@
+library("rpart")
+library("rpart.plot")
+library("dplyr")
+library("caret")
+library("RColorBrewer")
+library("ROCR")
+
+"
+View the tree
+issues with library(rattle)
+"
+
+
+# Import the development sample
+devSample <- read.csv("./data/DEV_SAMPLE.csv", sep = ",", header = TRUE)
+head(devSample)
+
+## Response Rate 
+responseRate <- sum(devSample$Target)/nrow(devSample)
+responseRate
+
+# specifying the criteria
+controlCriteria = rpart.control(minsplit=100, minbucket = 10, cp = 0, xval = 10)
+
+# build the decison tree based on CART
+classificationModel <- rpart(formula = Target ~ ., data = devSample[,-1], method = "class", control = controlCriteria)
+classificationModel
+fancyRpartPlot(classificationModel)
+
+# use the CP value where the xerror is the least
+printcp(classificationModel)
+plotcp(classificationModel)
+
+# prune the tree to avoid overfitting CP - 0.00202429
+classificationModel <- prune(classificationModel, cp = 0.0021 ,"CP")
+printcp(classificationModel)
+fancyRpartPlot(classificationModel)
+
+# predict the target for the development sample
+devSample$pTarget <- predict(classificationModel, devSample, type="class")
+predictionScore <- predict(classificationModel, devSample)
+devSample$pPositiveScore <- predictionScore[,2]
+
+" 
+Scoring the model based on the development sample
+lift chart to determine model performance
+Need help on how to draw charts: https://www.analyticsvidhya.com/blog/2016/02/7-important-model-evaluation-error-metrics/
+"
+devSample <- mutate(devSample, deciles = ntile(pPositiveScore, 10))
+View(devSample)
+sampleSize <- nrow(devSample)  
+responseRate <- (sum(devSample$Target)/sampleSize)*100
+noOfResponseForResponders <- sum(devSample$Target==1)
+noOfResponseForNonResponders <- sum(devSample$Target==0)
+devSampleByDecile <- group_by(devSample[,-1], deciles)
+devSampleByDecile <- summarise(devSampleByDecile, 
+                               n = n(), 
+                               resonders = sum(Target==1), 
+                               nonResponders = sum(Target==0), 
+                               pResponders = sum(pTarget==1),
+                               pNonResponders = sum(pTarget==0),   
+                               responseRateForNonResponders = (nonResponders/noOfResponseForNonResponders)*100,
+                               responseRateForResponders = (resonders/noOfResponseForResponders)*100
+                               )
+devSampleByDecile <- arrange(devSampleByDecile, desc(deciles))
+devSampleByDecile <- mutate(devSampleByDecile, 
+                            cumResponseRateForResponders = cumsum(responseRateForResponders),
+                            cumResponseRateForNonResponders = cumsum(responseRateForNonResponders),
+                            perPopulation = (n/sampleSize)*100,
+                            cumPerPopulation = cumsum(perPopulation),
+                            liftAtDecile = (responseRateForResponders/perPopulation)*100,
+                            totalLift = (cumResponseRateForResponders/cumPerPopulation)*100,
+                            ks = (cumResponseRateForResponders - cumResponseRateForNonResponders)
+                            )
+View(devSampleByDecile)
+
+"
+confusion matrix to determine model performance
+https://www.analyticsvidhya.com/blog/2016/02/7-important-model-evaluation-error-metrics/
+"
+confusionMatrix(devSample$pTarget, devSample$Target)
+
+"
+Kolomogorov Smirnov chart
+"
+plot(devSampleByDecile$deciles, devSampleByDecile$ks, type = "l")
+
+
+"
+Other methods to determine model performance
+ROC curve on the other hand is almost independent of the response rate
+AUC - Area under curve
+Gini Coffecient -
+"
+predition <- prediction(devSample$pPositiveScore, devSample$Target)
+tprFpr <- performance(predition, "tpr", "fpr")
+plot(tprFpr)
+KS <- max(attr(tprFpr, 'y.values')[[1]]-attr(tprFpr, 'x.values')[[1]])
+auc <- performance(predition,"auc"); 
+auc <- as.numeric(auc@y.values)
+giniCoffecient <- (2*auc)-1
+
+
+# Import the holdout sample
+holdOutSample <- read.csv("./data/HOLDOUT_SAMPLE.csv", sep = ",", header = TRUE)
+head(holdOutSample)
+
+# predict the target for the holdout sample
+holdOutSample$pTarget <- predict(classificationModel, holdOutSample, type="class")
+predictionScore <- predict(classificationModel, holdOutSample)
+holdOutSample$pPositiveScore <- predictionScore[,2]
+
+" 
+Scoring the model based on the development sample
+lift chart to determine model performance
+Need help on how to draw charts: https://www.analyticsvidhya.com/blog/2016/02/7-important-model-evaluation-error-metrics/
+"
+holdOutSample <- mutate(holdOutSample, deciles = ntile(pPositiveScore, 10))
+View(holdOutSample)
+sampleSize <- nrow(holdOutSample)  
+responseRate <- (sum(holdOutSample$Target)/sampleSize)*100
+noOfResponseForResponders <- sum(holdOutSample$Target==1)
+noOfResponseForNonResponders <- sum(holdOutSample$Target==0)
+holdOutSampleByDecile <- group_by(holdOutSample[,-1], deciles)
+holdOutSampleByDecile <- summarise(holdOutSampleByDecile, 
+                               n = n(), 
+                               resonders = sum(Target==1), 
+                               nonResponders = sum(Target==0), 
+                               pResponders = sum(pTarget==1),
+                               pNonResponders = sum(pTarget==0),   
+                               responseRateForNonResponders = (nonResponders/noOfResponseForNonResponders)*100,
+                               responseRateForResponders = (resonders/noOfResponseForResponders)*100
+)
+holdOutSampleByDecile <- arrange(holdOutSampleByDecile, desc(deciles))
+holdOutSampleByDecile <- mutate(holdOutSampleByDecile, 
+                            cumResponseRateForResponders = cumsum(responseRateForResponders),
+                            cumResponseRateForNonResponders = cumsum(responseRateForNonResponders),
+                            perPopulation = (n/sampleSize)*100,
+                            cumPerPopulation = cumsum(perPopulation),
+                            liftAtDecile = (responseRateForResponders/perPopulation)*100,
+                            totalLift = (cumResponseRateForResponders/cumPerPopulation)*100,
+                            ks = (cumResponseRateForResponders - cumResponseRateForNonResponders)
+)
+View(holdOutSampleByDecile)
+
+"
+Other methods to determine model performance
+ROC curve on the other hand is almost independent of the response rate
+AUC - Area under curve
+Gini Coffecient -
+"
+hPredition <- prediction(holdOutSample$pPositiveScore, holdOutSample$Target)
+hTprFpr <- performance(hPredition, "tpr", "fpr")
+plot(hTprFpr)
+hKS <- max(attr(hTprFpr, 'y.values')[[1]]-attr(hTprFpr, 'x.values')[[1]])
+hAuc <- performance(hPredition,"auc"); 
+hAuc <- as.numeric(hAuc@y.values)
+hGiniCoffecient <- (2*hAuc)-1
+
+"
+confusion matrix to determine model performance
+https://www.analyticsvidhya.com/blog/2016/02/7-important-model-evaluation-error-metrics/
+"
+confusionMatrix(devSample$pTarget, devSample$Target)
+confusionMatrix(holdOutSample$pTarget, holdOutSample$Target)
+
+"
+Comparing dev & hold out
+"
+
+KS
+hKS
+
+auc
+hAuc
+
+giniCoffecient
+hGiniCoffecient
--- Clustering.R
+++ Clustering.R
@@ -1,55 +1,48 @@
-## Author: Rajesh Jakhotia
-## Company Name: K2 Analytics Finishing School Pvt. Ltd
-## Email : ar.jakhotia@k2analytics.co.in
-## Website : k2analytics.co.in
-
 ## Let us find the clusters in given Retail Customer Spends data
 ## Hierarchical Clustering
 
 ## Let us first set the working directory path and import the data
 
-setwd ("D:/K2Analytics/datafile")
-getwd()
+customerSpend <- read.csv("./data/Cust_Spend_Data.csv", header=TRUE)
+View(customerSpend)
 
-RCDF <- read.csv("Cust_Spend_Data.csv", header=TRUE)
-View(RCDF)
-
-?dist  ## to get help on distance function
-d.euc <- dist(x=RCDF[,3:7], method = "euclidean") 
+# Determine the euclidean distance
+d.euc <- dist(x=customerSpend[,3:7], method = "euclidean") 
 d.euc
 
 ## we will use the hclust function to build the cluster
 ?hclust  ## to get help on hclust function
 
 clus1 <- hclust(d.euc, method = "average")
-plot(clus1, labels = as.character(RCDF[,2]))
+
+plot(clus1, labels = as.character(customerSpend[,2]))
 
 ## scale function standardizes the values
-scaled.RCDF <- scale(RCDF[,3:7])
+scaled.RCDF <- scale(customerSpend[,3:7])
 head(scaled.RCDF, 10)
 d.euc
 d.euc <- dist(x=scaled.RCDF, method = "euclidean") 
+d.euc
 clus2 <- hclust(d.euc, method = "average")
-plot(clus2, labels = as.character(RCDF[,2]))
+plot(clus2, labels = as.character(customerSpend[,2]))
 rect.hclust(clus2, k=4, border="red")
 clus2$height
 
-View(RCDF)
+View(customerSpend)
 ## profiling the clusters
-RCDF$Clusters <- cutree(clus2, k=3)
-aggr = aggregate(RCDF[,-c(1,2, 8)],list(RCDF$Clusters),mean)
-clus.profile <- data.frame( Cluster=aggr[,1],
-                            Freq=as.vector(table(RCDF$Clusters)),
-                            aggr[,-1])
-
-View(clus.profile)
+customerSpend$Clusters <- cutree(clus2, k=4)
+View(customerSpend)
 
 
+customer.profile <- group_by(customerSpend[,-c(1,2)], Clusters)
+customer.profile <- summarise(customer.profile, count=n(), mean(Avg_Mthly_Spend), mean(No_Of_Visits), mean(Apparel_Items), mean(FnV_Items), mean(Staples_Items))
+customer.profiles <- data.frame(customer.profile)
+View(customer.profile)
 
+  
 ## K Means Clustering
 
-
-KRCDF <- read.csv("Cust_Spend_Data.csv", header=TRUE)
+KRCDF <- read.csv("./data/Cust_Spend_Data.csv", header=TRUE)
 ## scale function standardizes the values
 scaled.RCDF <- scale(KRCDF[,3:7])
 
@@ -73,7 +66,7 @@ wssplot <- function(data, nc=15, seed=1234){
 wssplot(scaled.RCDF, nc=5)
 
 ## Identifying the optimal number of clusters
-##install.packages("NbClust")
+## install.packages("NbClust")
 
 library(NbClust)
 ?NbClust
@@ -83,11 +76,10 @@ nc <- NbClust(KRCDF[,c(-1,-2)], min.nc=2, max.nc=4, method="kmeans")
 table(nc$Best.n[1,])
 
 barplot(table(nc$Best.n[1,]),
-          xlab="Numer of Clusters", ylab="Number of Criteria",
-          main="Number of Clusters Chosen by 26 Criteria")
+        xlab="Numer of Clusters", ylab="Number of Criteria",
+        main="Number of Clusters Chosen by 26 Criteria")
 
 
-?kmeans
 kmeans.clus = kmeans(x=scaled.RCDF, centers = 3, nstart = 25)
 kmeans.clus
 
@@ -101,17 +93,13 @@ plotcluster(scaled.RCDF, kmeans.clus$cluster)
 
 # More complex
 library(cluster)
-?clusplot
 clusplot(scaled.RCDF, kmeans.clus$cluster, 
          color=TRUE, shade=TRUE, labels=2, lines=1)
 
 ## profiling the clusters
-KRCDF$Clusters <- kmeans.clus$cluster
-View(KRCDF)
-aggr = aggregate(KRCDF[,-c(1,2, 8)],list(KRCDF$Clusters),mean)
-clus.profile <- data.frame( Cluster=aggr[,1],
-                            Freq=as.vector(table(KRCDF$Clusters)),
-                            aggr[,-1])
-
-View(clus.profile)
+library("dplyr")
+groupByClusters <- group_by(KRCDF[,-c(1,2)], Clusters)
+summByClusters <- summarise(groupByClusters, count=n(), mean(Avg_Mthly_Spend), mean(No_Of_Visits), mean(Apparel_Items), mean(FnV_Items), mean(Staples_Items))
+summByClusters <- data.frame(summByClusters)
+summByClusters
 
