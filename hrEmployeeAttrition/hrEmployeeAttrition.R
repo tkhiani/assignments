4df985b40a473a8fe97545dcd6cf5eca305e3cde
Tarun Kamal Khiani <tarun.kamal.khiani@sap.com>
2017-07-04 06:05:31 +0530

    Employee Attrition

--- hrEmployeeAttrition/hrEmployeeAttrition.R
+++ hrEmployeeAttrition/hrEmployeeAttrition.R
@@ -6,14 +6,23 @@ library(caret)
 library(Deducer)
 library(dummies)
 library(ROCR)
+library(pROC)
+library(corrplot)
+library(ggplot2)
+library(gridExtra)
 
 # Load employee attrition data
 attritionData <- read.csv("./hrEmployeeAttrition/HR_Employee_Attrition_Data.csv")
+
+# Exploratory Data Analysis
 
 # Remove fields that are not relevant for classificaiton employee number and employee count
 head(attritionData)
 dim(attritionData)
 str(attritionData)
+summary(attritionData)
+describe(attritionData)
+describeBy(attritionData, group = attritionData$Attrition)
 
 # Convert factor fields to factors and scale numeric fields
 # Remove employee count, standard hours, over 18 as it is not relevant for analysis
@@ -30,10 +39,15 @@ attritionData$EmployeeCount <- NULL
 attritionData$StandardHours <- NULL
 attritionData$Over18 <- NULL
 
-for(i in factorFields) 
+for(i in factorFields) {
   attritionData[,i] <- as.factor(attritionData[,i])
+  if(i != "Attrition")
+    print(ggplot() + 
+                 geom_bar(aes(y = ..count..,x =attritionData[,i], fill = Attrition), 
+                          data = attritionData, position = position_stack()) + xlab(i))
+}
 
-attritionNumericData <- data.frame(scale(attritionData[,numericFields])) 
+attritionNumericData <- data.frame(scale(attritionData[,numericFields]))
 attritionFactorsData <- attritionData[,factorFields]
 attritionData <- data.frame(attritionFactorsData, attritionNumericData)
 
@@ -42,76 +56,80 @@ str(attritionData)
 rm(attritionFactorsData)
 rm(attritionNumericData)
 
-# Exploratory Data Analysis
-summary(attritionData)
-describe(attritionData)
-describeBy(attritionData, group = attritionData$Attrition)
+
+r <- round(cor(attritionData[,numericFields]),2)
+# The following are highly correlated
+# Age - Total Working Years
+# Total Working Years - Monthly Income, Age, Years at Company
+# Years In Current Role - Years at Company, Years with Current Manager
+# Years with current manager - Years in current role, Years at company
+corrplot(r, order = "hclust", tl.col='black', tl.cex=.75) 
 
 # Convernt no to 0 and yes to 1 for attrition
 attritionData$Attrition <- ifelse(attritionData$Attrition =="Yes", 1, 0)
 attritionData$Attrition <- as.factor(attritionData$Attrition)
+attritionRate <- sum(attritionData$Attrition == "1") / nrow(attritionData)
+attritionRate
 
-# Split the data into train & test
-train <- sample_frac(attritionData, 0.7) 
-test <- train[-as.numeric(rownames(train)),]
+# Split the data into dev & hOut
+set.seed(9090)
+dev <- sample_frac(attritionData, 0.7) 
+hOut <- attritionData[-as.numeric(rownames(dev)),]
 
 rm(attritionData)
 
 # Attrition Rate
-trainSize <- nrow(train)
-attritionRate <- sum(train$Attrition == "1") / trainSize
+devSize <- nrow(dev)
+attritionRate <- sum(dev$Attrition == "1") / devSize
 
-trainSize
+devSize
 attritionRate
 
 # RF Model to classify 
-nSize <- round(1*trainSize/100,0)
-describe(train)
-
-rfModel <- randomForest(train$Attrition~.,
-             data = train[,-1],
+nSize <- round(3*devSize/100,0)
+nSize
+rfModel <- randomForest(dev$Attrition~.,
+             data = dev[,-1],
              ntree = 500,
-             mtry = 20, 
+             mtry = 10, 
              nodesize = nSize,
              importance = TRUE)
 
 rfModel
-varImpPlot(rfModel)
 plot(rfModel)
 
 # Tune the RF model to determine the number of varialbles that need to be randomly sampled at each split
-tRFModel <- tuneRF(x = train[,-1], 
-              y = train$Attrition, 
+tRFModel <- tuneRF(x = dev[,-1], 
+              y = dev$Attrition, 
               mtryStart = 3, 
               stepFactor = 1.5, 
-              ntreeTry = 500, 
+              ntreeTry = 300, 
               nodeSize = nSize,
               doBest = TRUE,
               importance = TRUE,
               plot = TRUE,
-              improve = 0.0001,
+              improve = 0.001,
               trace = TRUE)
 
 varImpPlot(tRFModel)
 varImp(tRFModel)
-tRFModel$importance
 tRFModel$mtry
 
-# predict the attrition for the train data
-probAttritionTrainRF <- predict(tRFModel, train, type = "prob")
-pAttritionTrainRF <- predict(tRFModel, train, type = "response")
+# predict the attrition for the dev data
+probAttritiondevRF <- tRFModel$votes[,2]
+pAttritiondevRF <- tRFModel$predicted
 
-# rank to determine which decile and k-s value of the training set
-trainWithDecile <- train
-trainWithDecile$probAttritionTrain <- probAttritionTrainRF[,2]
-trainWithDecile <- mutate(trainWithDecile, deciles = ntile(trainWithDecile$probAttritionTrain, 10))
-trainWithDecile$pAttrition <- pAttritionTrainRF 
-sampleSize <- nrow(trainWithDecile)  
-totalLeft <- sum(trainWithDecile$Attrition == "1")
-totalRetained <- sum(trainWithDecile$Attrition == "0")
+# rank to determine which decile and k-s value of the deving set
+devWithDecile <- dev
+devWithDecile$probAttritiondev <- probAttritiondevRF
+devWithDecile <- mutate(devWithDecile, deciles = ntile(devWithDecile$probAttritiondev, 10))
+devWithDecile$pAttrition <- pAttritiondevRF 
+sampleSize <- nrow(devWithDecile)  
+totalLeft <- sum(devWithDecile$Attrition == "1")
+totalRetained <- sum(devWithDecile$Attrition == "0")
 
-trainByDecile <- group_by(trainWithDecile, deciles)
-trainByDecile <- summarise(trainByDecile, 
+devByDecile <- group_by(devWithDecile, deciles)
+devByDecile <- summarise(devByDecile, 
                                n = n(), 
                                left = sum(Attrition=="1"), 
                                retained = sum(Attrition=="0"), 
@@ -120,8 +138,8 @@ trainByDecile <- summarise(trainByDecile,
                                rateLeft = (left/totalLeft)*100,
                                rateRetained = (retained/totalRetained)*100
 )
-trainByDecile <- arrange(trainByDecile, desc(deciles))
-trainByDecile <- mutate(trainByDecile, 
+devByDecile <- arrange(devByDecile, desc(deciles))
+devByDecile <- mutate(devByDecile, 
                             cumRateRetained = cumsum(rateRetained),
                             cumRateLeft = cumsum(rateLeft),
                             perPopulation = (n/sampleSize)*100,
@@ -130,53 +148,102 @@ trainByDecile <- mutate(trainByDecile,
                             totalLift = (cumRateLeft/cumPerPopulation)*100,
                             ks = abs(cumRateLeft - cumRateRetained)
 )
-View(trainByDecile)
+View(devByDecile)
 
 # evaluate performance using a confusion matrix
-confusionMatrix(pAttritionTrainRF, train$Attrition, positive = "1")
+confusionMatrix(pAttritiondevRF, dev$Attrition, positive = "1")
 # evaluate performance using ROC curve, KS, AUC curve
-aucTRF <- roc(pAttritionTrainRF,probAttritionTrainRF[,2])
-plot(aucTRF)
-# Need to still look at others liks KS, Gini Index...
 
-# Predict for test data
-pAttritionTestRF <- predict(tRFModel, test, type = "response")
-probAttritionTestRF <- predict(tRFModel, test, type = "prob")
+# Plot the performance of the model applied to the evaluation set as
+# an ROC curve.
+pred <- prediction(probAttritiondevRF, dev$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
+aucRFdev <- roc(dev$Attrition,probAttritiondevRF)
+aucRFdev
+ksRFdev <- max(devByDecile$ks)
+ksRFdev
+plot(aucRFdev)
 
-# Evaludate performance for test data using a confusion matrix
-confusionMatrix(pAttritionTestRF, test$Attrition, positive = "1")
+# Predict for hOut data
+pAttritionhOutRF <- predict(tRFModel, hOut, type = "response")
+probAttritionhOutRF <- predict(tRFModel, hOut, type = "prob")
+
+# Evaludate performance for hOut data using a confusion matrix
+confusionMatrix(pAttritionhOutRF, hOut$Attrition, positive = "1")
+aucRFhOut <- roc(hOut$Attrition,probAttritionhOutRF[,2])
+aucRFhOut
+plot(aucRFhOut)
+pred <- prediction(probAttritionhOutRF[,2], hOut$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
 
 # Work on a Neural Network Model for which we need to create dummies as it does not take categorical data
-trainWithDummies <- dummy.data.frame(train[,-1], sep = ".")
-trainWithDummies <- setNames(trainWithDummies, make.names(names(trainWithDummies), unique = TRUE))
+devWithDummies <- dummy.data.frame(dev[,-1], sep = ".")
+devWithDummies <- setNames(devWithDummies, make.names(names(devWithDummies), unique = TRUE))
 
-trainWithDummies$Attrition <- as.integer(train$Attrition) - 1
-n <- names(trainWithDummies)
+devWithDummies$Attrition <- as.integer(dev$Attrition) - 1
+n <- names(devWithDummies)
 f <- as.formula(paste("Attrition ~", paste(n[!n %in% "Attrition"], collapse = " + ")))
+f
+nCols <- ncol(devWithDummies)
+nCols
 nModel <- neuralnet(f,
-          data = trainWithDummies,
-          hidden = c(49,7), 
-          threshold = 0.001,
+          data = devWithDummies,
+          hidden = 52, 
+          threshold = 0.00001,
           linear.output = FALSE,
           err.fct = "sse",
-          stepmax = 2000,
-          rep = 5)
+          stepmax = 3000,
+          lifesign = "full",
+          lifesign.step = 10)
 
-probAttritionTrainNN <- compute(nModel, trainWithDummies[,-1])
-pAttritionTrainNN <- round(probAttritionTrainNN$net.result, digits = 0)
-confusionMatrix(pAttritionTrainNN, train$Attrition, positive = "1")
+probAttritiondevNN <- compute(nModel, devWithDummies[,-1])
+pAttritiondevNN <- round(probAttritiondevNN$net.result, digits = 0)
+# performance of the NN model with dev data
+confusionMatrix(pAttritiondevNN[,1], dev$Attrition, positive = "1")
+aucNNdev <- roc(dev$Attrition,probAttritiondevNN$net.result)
+aucNNdev
+plot(aucNNdev)
+pred <- prediction(probAttritiondevNN$net.result, dev$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
 
-# Predict attrition on the test data and evaluate it's performance
-testWithDummies <- dummy.data.frame(test[,-1], sep = ".")
-testWithDummies <- setNames(testWithDummies, make.names(names(testWithDummies), unique = TRUE))
+# Predict attrition on the hOut data and evaluate it's performance
+hOutWithDummies <- dummy.data.frame(hOut[,-1], sep = ".")
+hOutWithDummies <- setNames(hOutWithDummies, make.names(names(hOutWithDummies), unique = TRUE))
 
-probAttritionTestNN <- compute(nModel, testWithDummies)
-pAttritionTestNN <- round(probAttritionTestNN$net.result, digits = 0)
-confusionMatrix(pAttritionTestNN, test$Attrition, positive = "1")
+probAttritionhOutNN <- compute(nModel, hOutWithDummies)
+pAttritionhOutNN <- round(probAttritionhOutNN$net.result, digits = 0)
+# Evaludate performance for hOut data 
+confusionMatrix(pAttritionhOutNN, hOut$Attrition, positive = "1")
+aucNNhOut <- roc(hOut$Attrition,probAttritionhOutNN$net.result)
+aucNNhOut
+plot(aucNNhOut)
+pred <- prediction(probAttritionhOutNN$net.result, hOut$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
 
-# Ensemble Model - averaging the probabilities as we only have two algorithms. If we had three I would have used a voting principle  
-jointProbTestData <- (probAttritionTestRF[,2] + probAttritionTestNN$net.result) / 2
-jointPAttritionTest <- round(jointProbTestData, digits = 0)
+# Ensemble Model - averaging the probabilities as we only have two algorithms.
+# If we had three I would have used a voting principle  
+jointProbdev <- (probAttritiondevRF + probAttritiondevNN$net.result) / 2
+jointPAttritiondev <- round(jointProbdev, digits = 0)
+
+# Evaluating the performance of the model for the dev data via a confusion matrix
+confusionMatrix(jointPAttritiondev, dev$Attrition, positive = "1")
+auchOut <- roc(dev$Attrition,jointProbdev)
+auchOut
+plot(auchOut)
+pred <- prediction(jointProbdev, dev$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
+
+
+# Ensemble Model - averaging the probabilities as we only have two algorithms.
+# If we had three I would have used a voting principle  
+jointProbhOut <- (probAttritionhOutRF[,2] + probAttritionhOutNN$net.result) / 2
+jointPAttritionhOut <- round(jointProbhOut, digits = 0)
 
 # Evaluating the performance of the model via a confusion matrix
-confusionMatrix(jointPAttritionTest, test$Attrition, positive = "1")
+confusionMatrix(jointPAttritionhOut, hOut$Attrition, positive = "1")
+auchOut <- roc(hOut$Attrition,jointProbhOut)
+auchOut
+plot(auchOut)
+pred <- prediction(jointProbhOut, hOut$Attrition)
+plot(performance(pred,"lift","rpp"), main="lift curve", colorize=T)
+
